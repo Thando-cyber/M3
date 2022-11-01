5929b2e2d462f9221f35a1be5306f79a0d648471
Ihsaan Bhamjee <220031695@stu.ukzn.ac.za>
2022-11-01 17:21:37 +0200

    readded classlists and formclass pages

--- M3Proj/FormClass.aspx.cs
+++ M3Proj/FormClass.aspx.cs
@@ -7,11 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace M3Proj
 {
-    public partial class FormClass : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+	public partial class FormClass : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
 
-        }
-    }
+		}
+	}
 }
